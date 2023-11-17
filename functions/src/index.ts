8903e04e89237140f0ec7240fee9a4e20854a25b
jasonbrown <jasonsbrown25@gmail.com>
2023-11-17 21:52:44 +0200

    chore: slight improvements and additional commentary

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,25 +8,28 @@
  * https://firebase.google.com/docs/extensions/publishers
  */
 
-import { firestore } from "firebase-functions/v1";
 import { PaystackClient } from "./paystack/client";
-import { 
-  InitializeTransactionResponse, 
-  PaystackEvent 
-} from "./paystack/types";
+import { InitializeTransactionResponse, PaystackEvent } from "./paystack/types";
+
+import { firestore } from "firebase-functions/v1";
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
+import * as crypto from "crypto";
 
 initializeApp();
 
-// TODO: replace with env variable
+// Initialise variables
 const secret = process.env.PAYSTACK_API_KEY || "";
-const paymentsCollection = process.env.PAYMENTS_COLLECTION || "payments"
+const paymentsCollection = process.env.PAYMENTS_COLLECTION || "payments";
 const defaultCurrency = process.env.DEFAULT_CURRENCY || "ZAR";
 
-const paystackClient = new PaystackClient(secret)
+const paystackClient = new PaystackClient(secret);
 
+// paymentCreated is triggers when a document is added to the payments collection.
+// The information in this document is used to initialize a payment with Paystack
+// and the corresponding authoization URL for the payment is written back to 
+// the document
 export const paymentCreated = firestore
   .document(`${paymentsCollection}/{paymentID}`)
   .onCreate(async (snapshot, context) => {
@@ -34,19 +37,13 @@ export const paymentCreated = firestore
 
     // In order to create a payment, we need the following information:
     // 1. The email of the buyer
-    // 2. The amount in the subdenomination of the desired curreny (i.e. If ZAR, in cents)
+    // 2. The amount of the purchase
     // 3. The currency
     const paymentDetails = snapshot.data();
 
     // Check that the payment has the necessary fields and initialize the transaction
-    // Check that the payment has the necessary fields
-    if (
-      !paymentDetails ||
-      !paymentDetails.email ||
-      !paymentDetails.amount
-    ) {
-      // Write the edirect authorization URL response data back to the payment
-      // for use by clients
+    if (!paymentDetails || !paymentDetails.email || !paymentDetails.amount) {
+      // Write an error to the document if there is a problem initializing the transaction
       try {
         await snapshot.ref.set(
           {
@@ -63,13 +60,13 @@ export const paymentCreated = firestore
     // Create the transaction request
     const transactionRequest = {
       email: paymentDetails.email,
-      amount: paymentDetails.amount*100,
+      amount: paymentDetails.amount * 100,
       currency: defaultCurrency,
       // The reference is the unique ID assigned to payments as tracked in your backend
       reference: context.params.paymentID,
     };
 
-    let transactionResponse: InitializeTransactionResponse
+    let transactionResponse: InitializeTransactionResponse;
     try {
       // Initialize the transaction
       transactionResponse = await paystackClient.initializeTransaction(
@@ -77,53 +74,66 @@ export const paymentCreated = firestore
       );
       console.log("Transaction initialized", transactionResponse);
 
-      // Write the edirect authorization URL response data back to the payment 
+      // Write the redirect authorization URL response data back to the payment
       // for use by clients
-      await snapshot.ref.set(
-        transactionResponse,
-        { merge: true }
-      );
+      await snapshot.ref.set(transactionResponse, { merge: true });
     } catch (error) {
       console.error("Failed to initialize transaction", error);
     }
   });
 
-export const handlePayment = functions.runWith({
-      // Ensure the function has enough memory and time
-      // to process large files
-      timeoutSeconds: 300,
-    }).https.onRequest(async (req, res) => {
-  try {
-    const event: PaystackEvent = req.body;
-
-    switch (event.event) {
-      case "charge.success":
-        // Assuming that we store the payment details in a collection 'payments'
-        // and the Paystack transaction ID is used as the document ID in Firestore
-        const paymentRef = getFirestore()
-          .collection(paymentsCollection)
-          .doc(event.data.reference.toString());
-
-        // Update the payment document with the successful charge details
-        await paymentRef.set(
-          {
-            status: "successful",
-            gateway_response: event.data.gateway_response,
-          },
-          { merge: true }
-        );
-
-        break;
-
-      default:
-        // Handle unhandled events
-        console.warn("Unhandled Paystack event: ", event.event);
-        break;
+// handlePayment is the webhook that Paystack will send events to
+// to acknowledge receirpt of payment. We will use this event
+// to set the status of the payment accordingly
+export const handlePayment = functions
+  .runWith({
+    // Ensure the function has enough memory and time
+    // to process large files
+    timeoutSeconds: 300,
+  })
+  .https.onRequest(async (req, res) => {
+    // Verify the origin
+    const hash = crypto
+      .createHmac("sha512", secret)
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+    if (hash != req.headers["x-paystack-signature"]) {
+      // The origin is not from Paystack, warn and return error
+      console.warn("Origin is not Paystack. Denying verification");
+      res.status(403).send("Access denied");
     }
 
-    res.status(200).send("Event processed");
-  } catch (error) {
-    console.error("Error handling Paystack event: ", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+    try {
+      const event: PaystackEvent = req.body;
+
+      switch (event.event) {
+        case "charge.success":
+          // Assuming that we store the payment details in a collection 'payments'
+          // and the Paystack transaction ID is used as the document ID in Firestore
+          const paymentRef = getFirestore()
+            .collection(paymentsCollection)
+            .doc(event.data.reference.toString());
+
+          // Update the payment document with the successful charge details
+          await paymentRef.set(
+            {
+              status: "successful",
+              gateway_response: event.data.gateway_response,
+            },
+            { merge: true }
+          );
+
+          break;
+
+        default:
+          // Handle unhandled events
+          console.warn("Unhandled Paystack event: ", event.event);
+          break;
+      }
+
+      res.status(200).send("Event processed");
+    } catch (error) {
+      console.error("Error handling Paystack event: ", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
